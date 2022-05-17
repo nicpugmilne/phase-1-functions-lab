52f7e6c8667fe35a2440f14daa22210492f9d361
Nicola Milne <nicolalouisemilne@gmail.com>
2022-05-17 09:50:23 -0600

    commit tree 3bed8c384fcf78b4acfdefa465ccf028f9e25268

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const destination = 42
+
+function distanceFromHqInBlocks(origin){
+    return Math.abs(origin - destination);
+}
+
+function distanceFromHqInFeet(origin){
+    let blocks = distanceFromHqInBlocks(origin);
+    return blocks * 264;
+};
+
+function distanceTravelledInFeet(origin, destination){
+    return (Math.abs(origin - destination)* 264);
+}
+
+// function calculatesFarePrice(origin, destintation){
+//     const distance = distanceTravelledInFeet(origin, destination);
+//     if (distance <= 400){
+//         return 0;
+//     } else if (distance > 400 && distance <=2000){
+//         return .02 * (distance - 400);
+//     } else if (distance > 2000 && distance < 2500){
+//         return 25;
+//     } else {
+//         return 'cannot travel that far';
+//     }
+// }
+
+function calculatesFarePrice (origin, destination) {
+    const distance = distanceTravelledInFeet(origin, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
